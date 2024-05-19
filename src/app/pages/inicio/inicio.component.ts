11c883d34b2e1d2355f83c726e6072b1fba06a60
rynehh <ryneh_h@outlook.com>
2024-05-18 22:01:49 -0600

    Onboarding & JQuery

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,24 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import introJs from 'intro.js'; 
+import $ from 'jquery'; 
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.css',
+  ]
 })
 export class InicioComponent implements OnInit {
 
-
   ngOnInit(): void {
+
+    $(document).ready(function(){
+
+    introJs().setOptions({
+      steps: [{
+        element: document.querySelector('#signin') as HTMLElement,
+        title: "👆",
+        intro: "Haz clic aquí para iniciar sesión."
+      }, {
+        element: document.querySelector('#regis') as HTMLElement,
+        title: "👆",
+        intro: "Haz clic aquí para crear una cuenta."
+      },{
+        element: document.querySelector('#sucur') as HTMLElement,
+        title: "👆",
+        intro: "Haz clic aquí para ver nuestras sucursales."
+      },{
+        element: document.querySelector('#promos') as HTMLElement,
+        title: "👆",
+        intro: "Haz clic aquí para ver nuestras promociones."
+      },{
+        element: document.querySelector('#carro') as HTMLElement,
+        title: "👆",
+        intro: "Haz clic aquí para ver el carrito de compra."
+      }]
+
+    }).start();
+
+    introJs().start();
+
+    $("#buttoncol").click(function(){
+      $("#navbarCollapse").slideToggle("slow");
+    });
+  
+  });
     
+  
   }
 
+  
   menuSections = [
     { title: 'Pollos', description: '¡Disfruta de un Pollo Loco!', imageUrl: '/assets/imagenes/pollo.png' },
-    { title: 'Combos', description: '¡Los mejores combos para disfrutar!', imageUrl: '/assets/imagenes/combo.png'},
+    { title: 'Combos', description: '¡Los mejores combos para disfrutar!', imageUrl: '/assets/imagenes/combo.png' },
     { title: 'Complementos', description: 'Para disfrutarse con tu pollo', imageUrl: '/assets/imagenes/comp.png' },
     { title: 'Bebidas', description: 'Refréscate con las mejores bebidas', imageUrl: '/assets/imagenes/soda.png' },
-    { title: 'Postres', description: '¡Acompañalo con un postre!', imageUrl: '/assets/imagenes/postre.png' },
+    { title: 'Postres', description: '¡Acompáñalo con un postre!', imageUrl: '/assets/imagenes/postre.png' },
     { title: '¡Tu último pedido!', description: '¿Disfrutaste tu última comida? ¡Vuelve a ordenarla!', imageUrl: '/assets/imagenes/pollo.png' }
-    
-];
+  ];
 }
